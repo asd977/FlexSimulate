a9444150ce598f68cd1562dedd66d190870cab44
asd977 <61139971+asd977@users.noreply.github.com>
2025-09-18 15:33:36 +0800

    Finalize VTK UI refactor

--- SchemeSettingsDialog.h
+++ SchemeSettingsDialog.h
@@ -3,13 +3,29 @@
 
 class QLineEdit;
 class QLabel;
+class QPushButton;
 
-class SchemeSettingsDialog : public QDialog {
+class SchemeSettingsDialog : public QDialog
+{
     Q_OBJECT
 public:
-    explicit SchemeSettingsDialog(const QString& schemeName, QWidget* parent=nullptr);
+    explicit SchemeSettingsDialog(const QString& schemeName,
+                                  const QString& workingDirectory = QString(),
+                                  bool allowDirectoryChange = true,
+                                  QWidget* parent = nullptr);
+
+    QString schemeName() const;
+    QString workingDirectory() const;
+    void setSchemeName(const QString& name);
+    void setWorkingDirectory(const QString& directory);
+
+private slots:
+    void browseForDirectory();
 
 private:
-    QLabel* m_title;
-    QLineEdit* m_nameEdit;
+    QLabel* m_title = nullptr;
+    QLineEdit* m_nameEdit = nullptr;
+    QLineEdit* m_directoryEdit = nullptr;
+    QPushButton* m_browseButton = nullptr;
+    bool m_directoryEditable = true;
 };
